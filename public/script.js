4c6a9efe09a9ae3f36e1fac776707cb037bd87f8
Knouy <SanquirgoTanguy@Gmail.Com>
2021-04-23 13:03:36 +0200

    cinematheque-nosql

---
+++ public/script.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA74mURdk57hFn0jB9NSFzv1MY69ymxNSU",
+  authDomain: "cinematheque-nosql.firebaseapp.com",
+  databaseURL: "https://cinematheque-nosql.firebaseio.com",
+  projectId: "cinematheque-nosql",
+  storageBucket: "cinematheque-nosql.appspot.com",
+  messagingSenderId: "937608774480",
+  appId: "APP_ID"
+};
